3283e7862b46d4c248dd8147370d727dff4537a2
Melvin Fengels <fengelsm@gmail.com>
2025-01-14 14:00:35 +0100

    better setup script

--- client-setup/setup.sh
+++ client-setup/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 HOSTNAME=$1
 
+sudo echo "@lxpanel --profile LXDE-pi" > /etc/xdg/lxsession/LXDE-pi/autostart
+sudo echo "@pcmanfm --desktop --profile LXDE-pi" >> /etc/xdg/lxsession/LXDE-pi/autostart
+sudo echo "@xscreensaver -no-splash" >> /etc/xdg/lxsession/LXDE-pi/autostart
+
 sudo echo "@xset s off" >> /etc/xdg/lxsession/LXDE-pi/autostart
 sudo echo "@xset -dpms" >> /etc/xdg/lxsession/LXDE-pi/autostart
 sudo echo "@xset s noblank" >> /etc/xdg/lxsession/LXDE-pi/autostart
